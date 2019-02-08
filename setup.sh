2607a8fdec441dae68b1a191da7a64a6cbba2908
hira <elt@every-little.com>
2019-02-08 19:31:46 +0900

    update setup script for openfst-1.6.9 (#1)

--- setup.sh
+++ setup.sh
@@ -6,12 +6,11 @@ set -eu
 # set up openfst
 mkdir -p tmp
 cd tmp
-wget http://www.openfst.org/twiki/pub/FST/FstDownload/openfst-1.3.4.tar.gz
-tar xvfz openfst-1.3.4.tar.gz
-cd openfst-1.3.4
-./configure --prefix=`readlink -m ../../local`
-make install
+wget http://www.openfst.org/twiki/pub/FST/FstDownload/openfst-1.6.9.tar.gz
+tar xvfz openfst-1.6.9.tar.gz
+cd openfst-1.6.9
+export CFLAGS="-O2"
+export CXXFLAGS="-O2"
+./configure --prefix=`readlink -m ../../local` --enable-python
+make LDFLAGS=-no-undefined install
 cd ../..
-
-# set up pyfst
-pip install pyfst --global-option=build_ext --global-option="-I`pwd`/local/include" --global-option="-L`pwd`/local/lib" --install-option="--prefix=`pwd`/local" --ignore-installed
